004ee90fff9882346c0083920480275ca95b595a
NicolasCardenasL <nicolascardenas0115@gmail.com>
2024-08-12 19:28:48 -0500

    header init

---
+++ src/styles/screenResolutions.js
@@ -0,0 +1,9 @@
+const  screenResolutions = {
+  minDeskResolution: '1920px',
+  maxTabletResolution: '1919px',
+  minTabletResolution: '1024px',
+  maxMobileResolution: '1023px',
+  minMobileResolution: '375px',
+}
+
+export default screenResolutions
