0a7ddac91084b5e4617220a6347544f2c3d406c1
NicolasCardenasL <nicolascardenas0115@gmail.com>
2024-08-19 13:35:49 -0500

    header completed (desk)'

--- src/styles/screenResolutions.js
+++ src/styles/screenResolutions.js
@@ -1,8 +1,8 @@
 const  screenResolutions = {
   minDeskResolution: '1920px',
   maxTabletResolution: '1919px',
-  minTabletResolution: '1024px',
-  maxMobileResolution: '1023px',
+  minTabletResolution: '1060px',
+  maxMobileResolution: '1059px',
   minMobileResolution: '375px',
 }
 
