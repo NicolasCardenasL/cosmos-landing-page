e65a5821c6a83c955d029d07ce0e39036a6df83c
NicolasCardenasL <nicolascardenas0115@gmail.com>
2024-09-11 20:13:40 -0500

    Previous releases shall be not taken into account as they were push reckless and without further consideration. Notes: *Main project directory's structured created. *Since the Header component has already been coded It may and shall stay the same.

--- src/styles/screenResolutions.js
+++
@@ -1,9 +0,0 @@
-const  screenResolutions = {
-  minDeskResolution: '1920px',
-  maxTabletResolution: '1919px',
-  minTabletResolution: '1060px',
-  maxMobileResolution: '1059px',
-  minMobileResolution: '375px',
-}
-
-export default screenResolutions
